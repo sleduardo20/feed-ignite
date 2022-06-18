a60b0b8ec77e6b4b649f38d2027d713019803d20
sleduardo20 <sl.eduardo20@gmail.com>
2022-06-18 12:32:32 -0300

    ✨ feat  - create Post component

---
+++ src/components/Post.tsx
@@ -0,0 +1,40 @@
+import styles from "./Post.module.css";
+
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img
+            className={styles.avatar}
+            src="https://avatars.githubusercontent.com/u/56615577?v=4"
+            alt="avatar"
+          />
+          <div className={styles.authorInfo}>
+            <strong>Eduardo Lima</strong>
+            <span>Front End Developer</span>
+          </div>
+        </div>
+        <time title="18 de junho as 12:13" dateTime="2022-06-18 00:00:00">
+          Publicado ha 1h
+        </time>
+      </header>
+
+      <div className={styles.content}>
+        <p>
+          <p> Fala galeraa 👋 </p>
+          <p>
+            Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
+            no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare
+            🚀
+          </p>
+          <p>
+            <a href="#">👉 jane.design/doctorcare</a>
+          </p>
+
+          <a href="#">#novoprojeto #nlw #rocketseat</a>
+        </p>
+      </div>
+    </article>
+  );
+}
