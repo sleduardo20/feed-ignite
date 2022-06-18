0830211094080739fb179300ff19f55090c975f9
sleduardo20 <sl.eduardo20@gmail.com>
2022-06-18 14:50:16 -0300

    🎨 styles - add responsiveness

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import { Header } from "./components/Header";
 import "./global.css";
+import styles from "./App.module.css";
+import { Sidebar } from "./components/Sidebar";
+import { Post } from "./components/Post";
 
 function App() {
   return (
     <div className="App">
       <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post />
+        </main>
+      </div>
     </div>
   );
 }
